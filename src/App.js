ddde12ca21a753964504b89dac003a2f2a1bf19f
David Choi <davidchoi@Davids-MacBook-Pro.local>
2020-06-17 00:45:10 -0700

    Add the sign-up form and page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home.js';
+import SignUp from './components/SignUp/SignUp.js';
+import SignIn from './components/SignIn/SignIn.js'
+import Dashboard from './components/Dashboard/Dashboard.js';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router> 
+   <Switch> 
+       <Route exact path='/' component={Home}/> 
+       <Route exact path='/signup' component={SignUp}/> 
+       <Route exact path='/signin' component={SignIn}/> 
+       <Route exact path='/dashboard' component={Dashboard}/> 
+    </Switch>
+  </Router>
   );
 }
 
