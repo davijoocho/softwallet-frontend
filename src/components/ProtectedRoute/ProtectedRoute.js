ae673afd97c3e5114854b5e33d32875bc96fd824
David Choi <choidavid478@gmail.com>
2020-06-25 21:06:51 -0700

    Fix styling issues

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Route, Redirect} from 'react-router-dom';
 
+
  
 const ProtectedRoute = ({component: Component, isSignedIn, ...rest}) => {
+
     return( 
         <Route {...rest} render={props => (
-        isSignedIn===true ?
-        <Component {...rest}/> :
+        isSignedIn === true ?
+        <Component {...props}/> :
         <Redirect to='/signin'/>
         )}/>
     );
