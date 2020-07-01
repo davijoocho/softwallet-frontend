6de550c96f81aa9a230c8e36b5c261146d4474d4
David Choi <choidavid478@gmail.com>
2020-06-30 20:49:22 -0700

    Add main dashboard and navigation routes for budget categories

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -5,6 +5,8 @@ import Income from '../Income/Income.js';
 import Assets from '../Assets/Assets.js';
 import Liabilities from '../Liabilities/Liabilities.js';
 import Expenses from '../Expenses/Expenses.js';
+import {CssBaseline, AppBar, Typography} from '@material-ui/core';
+import './dashboard.css';
 
 
  
@@ -13,18 +15,35 @@ const ProtectedRoute = ({component: Component, isSignedIn, userProfile, ...rest}
    
 
     return( 
-        <div>
+        <div className='dashboard-root'>
+        <CssBaseline/>
+
+        <nav className='app-bar'>
+         <AppBar position='fixed'>
+           <Typography variant='h4'>Dashboard</Typography>
+         </AppBar>
+        </nav>
+
+        <div className='content-page-container'>
+
+        <div className='dashboard-drawer'>
         <Route {...rest} render={props => (
             isSignedIn === true ?
             <Component {...props}/> :
             <Redirect to='/signin'/>
          )}/>
+         </div>
         
+        <main className='content-page'>
         <Route exact path='/dashboard' render={props => (<Summary {...props}/>)}/>
         <Route exact path='/dashboard/income' render={props => (<Income {...props}/>)}/>
         <Route exact path='/dashboard/assets' render={props => (<Assets {...props}/>)}/>
         <Route exact path='/dashboard/liabilities' render={props => (<Liabilities {...props}/>)}/>
         <Route exact path='/dashboard/expenses' render={props => (<Expenses {...props}/>)}/>
+        </main> 
+
+        </div>
+
         </div>
     );
 }
