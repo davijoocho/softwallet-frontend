7c9b40006d6e521371a8e9141ad7561071232797
David Choi <choidavid478@gmail.com>
2020-06-30 20:47:54 -0700

    Add drawer for dashboard page

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Drawer, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
+import {AccountBalance, AttachMoney, HomeWork, CreditCard, AccountBalanceWallet} from '@material-ui/icons';
 
-const Dashboard = () => {
+const Dashboard = ({history}) => {
+
+    const [selectedText, setSelectedText] = useState('Summary');
+
+
+    const categoriesList = [
+        {
+            text: 'Summary',
+            icon: <AccountBalance/>,
+            onClick: () => {
+                history.push('/dashboard')
+                setSelectedText('Summary')
+            }
+        },
+        {
+            text: 'Income',
+            icon: <AttachMoney/>,
+            onClick: () => {
+                history.push('/dashboard/income')
+                setSelectedText('Income')
+            }
+        },
+        {
+            text: 'Assets',
+            icon: <HomeWork/>,
+            onClick: () => {
+                history.push('/dashboard/assets')
+                setSelectedText('Assets')
+            }
+        },
+        {
+            text: 'Liabilities',
+            icon: <CreditCard/>,
+            onClick: () => {
+                history.push('/dashboard/liabilities')
+                setSelectedText('Liabilities')
+            }
+        },
+        {
+            text: 'Expenses',
+            icon: <AccountBalanceWallet/>,
+            onClick: () => {
+                history.push('/dashboard/expenses')
+                setSelectedText('Expenses')
+            }
+        }
+    ]
 
 
     return (
-        <div>
+        <Drawer variant='permanent'>
 
-        <h1> Dashboard</h1>
+            <List> 
+                {
+                    categoriesList.map(item => {
 
+                        const {text, icon, onClick} = item;
 
-        </div>
+                        return (
+                        <ListItem selected={ text === selectedText ? true : false } onClick={onClick} button={true}>
+                            <ListItemIcon>{icon}</ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    );
+                })
+                }
+            </List>
+      
+
+        </Drawer>
     );
     
 }
